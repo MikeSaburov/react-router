8714a87a55d53fbce976d4c6c6f6081e910a5ce9
Mikhail Saburov <saburov1985@gmail.com>
2023-11-18 23:40:51 +1100

    refactor(App): реализован перход по страницам без перезагрузки

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import { Homepage } from './components/Homepage';
+import { Aboutpage } from './components/Aboutpage';
+import { Blogpage } from './components/Blogpage';
+import { Notfoundpage } from './components/Notfoundpage';
+
 function App() {
   return (
     <>
       <header>
-        <a href="/">Home</a>
-        <a href="/posts">Blog</a>
-        <a href="/about">About</a>
+        <Link to="/">Home</Link>
+        <Link to="/posts">Blog</Link>
+        <Link to="/about">About</Link>
       </header>
-      <div>
-        <h1>Get started with React-Router 6</h1>
-      </div>
+
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="posts" element={<Blogpage />} />
+        <Route path="about" element={<Aboutpage />} />
+        <Route path="*" element={<Notfoundpage />} />
+      </Routes>
     </>
   );
 }
