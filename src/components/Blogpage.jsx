2920a10e153dc699ec8239e82b64fcbc842d1283
Mikhail Saburov <saburov1985@gmail.com>
2023-11-18 23:19:14 +1100

    feature(components): создал страницы

---
+++ src/components/Blogpage.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Blogpage = () => {
+  return <div>Blogpage</div>;
+};
---
+++ src/components/Notfoundpage.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Notfoundpage = () => {
+  return <div>Notfoundpage</div>;
+};
