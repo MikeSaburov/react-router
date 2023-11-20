5dd41d595ca4f367aa0be989b1f86a11bb490113
Mikhail Saburov <saburov1985@gmail.com>
2023-11-20 22:43:49 +1100

    refactor(App): подключил Layout с Outlet

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, Outlet } from 'react-router-dom';
 import { Homepage } from './components/Homepage';
 import { Aboutpage } from './components/Aboutpage';
 import { Blogpage } from './components/Blogpage';
 import { Notfoundpage } from './components/Notfoundpage';
+import { Layout } from './components/Layout';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/posts" element={<Blogpage />} />
-        <Route path="/about" element={<Aboutpage />} />
-        <Route path="*" element={<Notfoundpage />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/posts" element={<Blogpage />} />
+          <Route path="/about" element={<Aboutpage />} />
+          <Route path="*" element={<Notfoundpage />} />
+        </Route>
       </Routes>
     </>
   );
