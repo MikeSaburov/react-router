2ca7f714b265cd751b0d6b3687dfd5e0a8c67033
Mikhail Saburov <saburov1985@gmail.com>
2023-11-21 22:42:33 +1100

    refactor(Blogpage): добавлены ссылки на заголовки постов

--- src/pages/Blogpage.jsx
+++ src/pages/Blogpage.jsx
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Blogpage = () => {
   const [posts, setPosts] = useState([]);
   useEffect(() => {
     axios({
-      url: 'https://jsonplaceholder.typicode.com/posts?_limit=10',
+      url: 'https://jsonplaceholder.typicode.com/posts?_limit=50',
     })
       .then((response) => {
         setPosts(response.data);
@@ -19,12 +20,10 @@ export const Blogpage = () => {
 
   return (
     <div>
-      {posts.map((obj) => (
-        <div className="container">
-          <h3>{obj.title}</h3>
-          <hr />
-          <p>{obj.body}</p>
-        </div>
+      {posts.map((post) => (
+        <Link key={post.id} to={`/posts/${post.id}`}>
+          <li>{post.title}</li>
+        </Link>
       ))}
     </div>
   );
