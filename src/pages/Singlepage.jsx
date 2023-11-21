2e9090b9f996b3a4306dc2250669b22bf0f19fe4
Mikhail Saburov <saburov1985@gmail.com>
2023-11-21 22:50:47 +1100

    feature(Singlepage): динамически отображаем пост

--- src/pages/Singlepage.jsx
+++ src/pages/Singlepage.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-
+import axios from 'axios';
 export const Singlepage = () => {
   const { id } = useParams();
-  return <div>{id}</div>;
+  const [post, setPost] = useState(null);
+  useEffect(() => {
+    axios({
+      url: `https://jsonplaceholder.typicode.com/posts/${id}?_limit=50`,
+    })
+      .then((response) => {
+        setPost(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert('не удалось получить данные');
+      });
+  }, [id]);
+  return (
+    <div>
+      {post && (
+        <>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+        </>
+      )}
+    </div>
+  );
 };
