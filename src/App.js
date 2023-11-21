5a13a7513c657a1772190bf6178220b36a0c7206
Mikhail Saburov <saburov1985@gmail.com>
2023-11-21 21:53:12 +1100

    refactor: вынес страницы в папку page и создал новую страницу Singlepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
-import { Homepage } from './components/Homepage';
-import { Aboutpage } from './components/Aboutpage';
-import { Blogpage } from './components/Blogpage';
-import { Notfoundpage } from './components/Notfoundpage';
+import { Homepage } from './pages/Homepage';
+import { Aboutpage } from './pages/Aboutpage';
+import { Blogpage } from './pages/Blogpage';
+import { Notfoundpage } from './pages/Notfoundpage';
 import { Layout } from './components/Layout';
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Homepage />} />
           <Route path="posts" element={<Blogpage />} />
+          <Route path="posts/:id" element={<Blogpage />} />
           <Route path="about" element={<Aboutpage />} />
           <Route path="*" element={<Notfoundpage />} />
         </Route>
