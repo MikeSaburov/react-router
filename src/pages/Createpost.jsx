a65e876c02426108160394b80b45546f388da2d7
Mikhail Saburov <saburov1985@gmail.com>
2023-11-30 18:58:59 +1100

    feature(Createpost): реализован выход
    если нажать на кнопку Выйти, то происходит редирект на главную и авторизация
    сбрасывается

--- src/pages/Createpost.jsx
+++ src/pages/Createpost.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { useAuth } from '../hook/useAuth';
+import { useNavigate } from 'react-router-dom';
 
 export const Createpost = () => {
-  return <div>Создать пост</div>;
+  const { singout } = useAuth();
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Создать пост</h1>
+      <button onClick={() => singout(() => navigate('/', { replace: true }))}>
+        Выйти
+      </button>
+    </div>
+  );
 };
