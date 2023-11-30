3937e5434447a5e606fb6e47bcd37243d62f8c90
Mikhail Saburov <saburov1985@gmail.com>
2023-11-30 18:52:57 +1100

    feature(Loginpage): Добавили форму для входа

--- src/hoc/RequireAuth.jsx
+++ src/hoc/RequireAuth.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export const RequireAuth = ({ children }) => {
   const location = useLocation();
-  const user = useAuth();
+  const { user } = useAuth();
 
   if (!user) {
     return <Navigate to="/login" state={{ from: location }} />;
--- src/pages/Loginpage.jsx
+++ src/pages/Loginpage.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
+import { useAuth } from '../hook/useAuth';
+
 export const LoginPage = () => {
   const location = useLocation();
   const navigate = useNavigate();
+  const { singin } = useAuth();
 
   const fromPage = location.state?.from?.pathname || '/';
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const user = form.username.value;
+
+    singin(user, () => navigate(fromPage, { replace: true }));
+  };
 
   return (
     <div>
       <h1>Login page</h1>
-      {fromPage}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name: <input type="text" name="username" />
+        </label>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 };
