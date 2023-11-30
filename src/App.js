44cb9327b1b15ed1eaf237a4a9b436ef53b680f6
Mikhail Saburov <saburov1985@gmail.com>
2023-11-30 17:41:12 +1100

    refactor(App): добавлен редирект на /about

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Homepage } from './pages/Homepage';
 import { Aboutpage } from './pages/Aboutpage';
 import { Blogpage } from './pages/Blogpage';
@@ -19,6 +19,7 @@ function App() {
           <Route path="posts/:id/edit" element={<Editpost />} />
           <Route path="posts/new" element={<Createpost />} />
           <Route path="about" element={<Aboutpage />} />
+          <Route path="about-us" element={<Navigate to="/about" replace />} />
           <Route path="*" element={<Notfoundpage />} />
         </Route>
       </Routes>
